06ac804beded8a1ab4fc5aaeb06c20960d4c9b6e
lepu <lepu@LEPU-20180713JN>
2018-12-03 16:00:42 +0800

    2018-12-3 16:00:39

---
+++ src/controller/test.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class test {
+	
+	@RequestMapping(value="/aaa")
+	public String aaa() {
+		return "login";
+	}
+}
